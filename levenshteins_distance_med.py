8e576eb6f126d917f74ff486f730cf2dc96077b7
Ricky <rickdhanota@gmail.com>
2020-10-25 22:10:18 -0700

    initial commit

---
+++ levenshteins_distance_med.py
@@ -0,0 +1,11 @@
+# Levenshteins distance
+
+#write a function that takes in two strings and returns the minimum number of edit operatiosnt hat need to be preformed on the first string to obtain the second string
+
+#There are three edit operations: insertions of a character, deletion of a character, and substitution of a character for another
+
+#sample input: str1 = "abc", str2 = "yabd"
+#output: 2 // insert "y"; substitute "c" for "d"
+
+def levenshteinsDistance(str1, str2):
+    pass
